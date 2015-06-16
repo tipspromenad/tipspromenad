24fac0a1d5a4f879ca74799ca9509569024b4d67
Wictor Johansson <WictorJ@Wictors-MBP.lan>
2015-06-16 18:37:14 +0200

    ändrade @admin i index.blade

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-    @role('Administrator')
+    @role('Admin')
         <div class="row">
             <div class="col-md-10 col-md-offset-1">
                 <div class="panel panel-default">
