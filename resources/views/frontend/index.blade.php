ba38b814f9960e538fd40e1ce658acd2a299c421
blomdahldaniel <blomdahl.daniel@gmail.com>
2015-06-17 02:11:08 +0200

    tog bort @role(Admin) i frontend/index

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -17,19 +17,6 @@
 @endsection
 
 @section('content')
-
-    @role('Admin')
-        <div class="row">
-            <div class="col-md-10 col-md-offset-1">
-                <div class="panel panel-default">
-                    <div class="panel-heading"><i class="fa fa-home"></i> Role Based</div>
-                    <div class="panel-body">
-                        You can see this because you have the role of 'Administrator'!
-                    </div>
-                </div><!-- panel -->
-            </div><!-- col-md-10 -->
-        </div><!-- row -->
-    @endrole
     <div class="row center-block siffror">
         <div class="col-xs-12">
             <img src="{!!asset('img/frontend/ettakrysstva.svg')!!}" class=”img-responsive" alt=”Etta, kryss, två!" />
