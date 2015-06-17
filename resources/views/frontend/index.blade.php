95ad2b0c4b5fd758806cd15f805bf2ba17e20847
Wictor Johansson <wictor.johansson@gmail.com>
2015-06-17 18:33:16 +0200

    added copyright information

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -28,4 +28,12 @@
             </div>
         </div>
     </div><!--  /.row -->
+    <div class="row">
+        <div class="col-xs-10 col-xs-offset-1">
+            <div class="text-center">
+                <hr>
+                <p><i class="fa fa-copyright"></i> Daniel Blomdahl & Wictor Johansson</p>
+            </div>
+        </div>
+    </div>
 @endsection
