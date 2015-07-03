62b0768c6fbdf99fa42dc5cbff65e76065e457fc
blomdahldaniel <blomdahl.daniel@gmail.com>
2015-07-03 15:43:26 +0200

    hittade ännu ett konstigt " som jag ändrade i index.blade.php

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -79,7 +79,7 @@
     <div class="row">
         <div class="col-xs-12">
             <div class="img-responsive text-center">
-                <img src="{!!asset('img/frontend/blurrybild.png')!!}" style="max-width: 80%; margin-top: 20px; margin-bottom: 40px;" alt=”Bild med suddig skog." />
+                <img src="{!!asset('img/frontend/blurrybild.png')!!}" style="max-width: 80%; margin-top: 20px; margin-bottom: 40px;" alt="Bild med suddig skog." />
             </div>
         </div>
     </div>
