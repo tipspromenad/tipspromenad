9c34351059fabb4a880d0e08dcffd6dfbd1e1e14
Wictor Johansson <wictor.johansson@gmail.com>
2015-07-08 12:41:03 +0200

    ändrat typsnitt på instructions-svg:en

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -41,7 +41,7 @@
                 </div><!--// col -->
                 <div class="col-xs-12 col-sm-10 col-sm-offset-1 col-lg-8 col-lg-offset-2 text-center">
                     <div class="img-responsive text-center">
-                        <img src="{!!asset('img/frontend/instructions.svg')!!}" alt="Steg för steg." />
+                        <img src="{!!asset('img/frontend/instructions2.svg')!!}" alt="Steg för steg." />
                     </div>
                 </div><!--// col -->
             </div><!--// row -->
