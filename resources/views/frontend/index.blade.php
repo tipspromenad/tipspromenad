9c4a80a021f96d671ff357856408ed5bd699b9d9
Wictor Johansson <wictor.johansson@gmail.com>
2015-07-08 16:17:15 +0200

    ändrade SVGn till en PNG istället

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -40,8 +40,8 @@
                     </div>
                 </div><!--// col -->
                 <div class="col-xs-12 col-sm-10 col-sm-offset-1 col-lg-8 col-lg-offset-2 text-center">
-                    <div class="img-responsive text-center">
-                        <img src="{!!asset('img/frontend/instructions2.svg')!!}" alt="Steg för steg." />
+                    <div class="text-center">
+                        <img class="img-responsive" src="{!!asset('img/frontend/instructionsTEST.png')!!}" alt="Steg för steg." />
                     </div>
                 </div><!--// col -->
             </div><!--// row -->
@@ -57,33 +57,7 @@
             </div><!--// row -->
         </div><!--// container -->
     </section> <!--// konferensbild -->
-
 <div class="container-fluid">
-
-    <div class="row">
-        <div class="col-xs-12">
-            <div class="siffror center-block">
-                <img src="{!!asset('img/frontend/ettakrysstva.svg')!!}" class=”img-responsive" alt=”Etta, kryss, två!" />
-            </div>
-        </div>
-    </div><!--  /.row -->
-
-    <div class="row">
-        <div class="col-xs-12">
-            <div class="logo center-block">
-                <a href="http://tipspromenad.dev" title="Klicka för att komma till startsidan."><img src="{!!asset('img/frontend/tipspromenadlogo.svg')!!}" class=”img-responsive" alt=”tipspromenad.NU" /></a>
-            </div>
-        </div>
-    </div><!--  /.row -->
-
-    <div class="row">
-        <div class="col-xs-12">
-            <div class="img-responsive text-center">
-                <img src="{!!asset('img/frontend/blurrybild.png')!!}" style="max-width: 80%; margin-top: 20px; margin-bottom: 40px;" alt="Bild med suddig skog." />
-            </div>
-        </div>
-    </div>
-
     <div class="row">
         <div class="col-xs-10 col-xs-offset-1">
             <div class="text-center">
