b2c8b18462a85bfb2e9f180a55b8d343beeef51e
Wictor Johansson <wictor.johansson@gmail.com>
2015-07-13 18:50:40 +0200

    autoreload prep

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -41,7 +41,7 @@
                 <h4 class="modal-title">Skapa tipspromenad</h4>
               </div>
               <div class="modal-body">
-                <p>HALLÅ VÄRLDEN!</p>
+
               </div>
               <div class="modal-footer">
                 <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
