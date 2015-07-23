bccd941f4b49a24d857d1bfdce6defa040da36aa
Wictor Johansson <wictor.johansson@gmail.com>
2015-07-23 09:06:03 +0200

    en sista indentering fixad..

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -162,12 +162,12 @@
     <div class="modal fade" id="skapautankonto" role="dialog">
         <div class="modal-dialog">
             <div class="modal-content">
-              <div class="modal-header bg-primary">
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-                <h4 class="modal-title">Skapa tipspromenad utan konto</h4>
-              </div>
+                <div class="modal-header bg-primary">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                    <h4 class="modal-title">Skapa tipspromenad utan konto</h4>
+                </div>
                 <div class="modal-body">
                     <div class="container-fluid">
                         <div class="row">
