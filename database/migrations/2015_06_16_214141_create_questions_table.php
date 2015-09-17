79efba591af94a7d28d7a3b1fa235143cb5c2902
blomdahldaniel <blomdahl.daniel@gmail.com>
2015-09-17 18:46:42 +0200

    added key "private" to Question::class

--- database/migrations/2015_06_16_214141_create_questions_table.php
+++ database/migrations/2015_06_16_214141_create_questions_table.php
@@ -15,6 +15,7 @@ class CreateQuestionsTable extends Migration
         Schema::create('questions', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('user_id')->nullable();
+            $table->boolean('private');
             $table->string('name');
             $table->text('question');
             $table->text('answer1');
